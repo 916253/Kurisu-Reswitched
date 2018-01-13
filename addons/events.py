7ccd2fdfefbd22b4461a5c449f83cd70f989e321
Ian Burgwin <ian@ianburgwin.net>
2018-01-13 03:14:28 -0800

    auto-lockdown channels if everyone is mentioned

--- addons/events.py
+++ addons/events.py
@@ -15,8 +15,6 @@ class Events:
         self.bot = bot
         print('Addon "{}" loaded'.format(self.__class__.__name__))
 
-
-
     # I hate naming variables sometimes
     user_antispam = {}
     channel_antispam = {}
@@ -35,7 +33,23 @@ class Events:
         embed = discord.Embed()
         embed.description = message.content
 
-
+    async def on_message(self, message):
+        if message.mention_everyone:
+            channel = message.channel
+            overwrites = channel.overwrites_for(self.bot.everyone_role)
+            ow_old_value1 = overwrites.send_messages
+            ow_old_value2 = overwrites.add_reactions
+            if ow_old_value1 is False:
+                return
+            overwrites.send_messages = False
+            overwrites.add_reactions = False
+            await self.bot.edit_channel_permissions(channel, self.bot.everyone_role, overwrites)
+            await self.bot.send_message(self.bot.modlogs_channel, "🔒 **Auto-Lockdown**: {} auto-locked due to mention of everyone".format(channel.mention))
+            await asyncio.sleep(60 * 2)
+            overwrites.send_messages = ow_old_value1
+            overwrites.add_reactions = ow_old_value2
+            await self.bot.edit_channel_permissions(channel, self.bot.everyone_role, overwrites)
+            await self.bot.send_message(self.bot.modlogs_channel, "🔒 **Auto-Unlock**: {}".format(channel.mention))
 
 
 def setup(bot):
