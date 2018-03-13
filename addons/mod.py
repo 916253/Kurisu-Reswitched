62e2273520d2e8c107d7102e2f70e9d4d67b5e23
Ian Burgwin <ian@ianburgwin.net>
2018-03-13 12:02:32 -0700

    put welcome footer into a tuple, minor cleanup (close #6)

--- addons/mod.py
+++ addons/mod.py
@@ -43,7 +43,7 @@ welcome_rules = (
     # 5
     """
     "Don't spam.
-     • For excessively long text, use a service like https://0bin.net/.
+     • For excessively long text, use a service like <https://0bin.net/>.
     """,
 
     # 6
@@ -59,34 +59,39 @@ welcome_rules = (
     'Absolutely no piracy. There is a zero-tolerance policy and we will enforce this strictly and swiftly.',
 )
 
-welcome_footer = """
-:hash: __Channel Breakdown:__
-#news - Used exclusively for updates on ReSwitched progress and community information. Most major announcements are passed through this channel and whenever something is posted there it's usually something you'll want to look at.
+welcome_footer = (
+    """
+    :hash: __Channel Breakdown:__
+    #news - Used exclusively for updates on ReSwitched progress and community information. Most major announcements are passed through this channel and whenever something is posted there it's usually something you'll want to look at.
 
-#vvv-faq - Answers to most frequently asked questions on this server. You'll probably want to read through this.
+    #vvv-faq - Answers to most frequently asked questions on this server. You'll probably want to read through this.
 
-#switch-hacking-meta - For "meta-discussion" related to hacking the switch. This is where we talk *about* the switch hacking that's going on, and where you can get clarification about the hacks that exist and the work that's being done.
+    #switch-hacking-meta - For "meta-discussion" related to hacking the switch. This is where we talk *about* the switch hacking that's going on, and where you can get clarification about the hacks that exist and the work that's being done.
 
-#user-support - End-user focused support, mainly between users. Ask your questions about using switch homebrew here.
+    #user-support - End-user focused support, mainly between users. Ask your questions about using switch homebrew here.
 
-#tool-support - Developer focused support. Ask your questions about using PegaSwitch, libtransistor, Mephisto, and other tools here.
+    #tool-support - Developer focused support. Ask your questions about using PegaSwitch, libtransistor, Mephisto, and other tools here.
 
-#hack-n-all - General hacking, hardware and software development channel for hacking on things *other* than the switch. This is a great place to ask about hacking other systems-- and for the community to have technical discussions.
+    #hack-n-all - General hacking, hardware and software development channel for hacking on things *other* than the switch. This is a great place to ask about hacking other systems-- and for the community to have technical discussions.
 
-#exchange - For hunting switches. People selling switches on low firmwares, and discussion about where to find homebrewable switches should go there.
+    #exchange - For hunting switches. People selling switches on low firmwares, and discussion about where to find homebrewable switches should go there.
+    """,
 
-#switch-hacking-general - Channel for everyone working on hacking the switch-- both in an exploit and a low-level hardware sense. This is where a lot of our in-the-open development goes on. Note that this isn't the place for developing homebrew-- we have #homebrew-development for that!
+    """
+    #switch-hacking-general - Channel for everyone working on hacking the switch-- both in an exploit and a low-level hardware sense. This is where a lot of our in-the-open development goes on. Note that this isn't the place for developing homebrew-- we have #homebrew-development for that!
 
-#homebrew-development - Discussion about the development of homebrew goes there. Feel free to show off your latest creation here.
+    #homebrew-development - Discussion about the development of homebrew goes there. Feel free to show off your latest creation here.
 
-#off-topic - Channel for discussion of anything that doesn't belong in #general. Anything goes, so long as you make sure to follow the rules and be on your best behavior.
+    #off-topic - Channel for discussion of anything that doesn't belong in #general. Anything goes, so long as you make sure to follow the rules and be on your best behavior.
 
-#toolchain-development - Discussion about the development of libtransistor itself goes there.
+    #toolchain-development - Discussion about the development of libtransistor itself goes there.
 
-#cfw-development - Development discussion regarding custom firmware (CFW) projects, such as Atmosphère. This channel is meant for the discussion accompanying active development.
+    #cfw-development - Development discussion regarding custom firmware (CFW) projects, such as Atmosphère. This channel is meant for the discussion accompanying active development.
 
-**When you have read everything, send a message in this channel to ask us to give you access to the other channels. Have a nice day!** \n **Note: We know if you haven't read everything.  We will kick the first time, ban the second.**
-"""
+    **When you have read everything, send a message in this channel to ask us to give you access to the other channels. Have a nice day!**
+    **Note: We know if you haven't read everything.  We will kick the first time, ban the second.**
+    """,
+)
 
 hidden_term_line = ' • When you are asking us to give you access to the other channels, please include "{}" in your message. Failure to do so may result in being kicked out.'
 
@@ -179,7 +184,8 @@ class Mod:
             await self.bot.send_message(self.bot.modlogs_channel, msg)
             # this should eventually generate messages to send, instead of sending it all at once which will be a problem when it goes above 2,000 characters
             await self.bot.say('\n\n'.join(rules))
-            await self.bot.say(welcome_footer)
+            for x in welcome_footer:
+                await self.bot.say(cleandoc(x))
         except discord.errors.Forbidden:
             await self.bot.say("💢 I don't have permission to do this.")
 
