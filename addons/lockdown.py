0bb84e821a2a83eb1b5f5d1c3586491333310eaf
Ian Burgwin <ian@ianburgwin.net>
2018-03-14 20:29:29 -0700

    allow "cm" to lock, other changes

--- addons/lockdown.py
+++ addons/lockdown.py
@@ -16,14 +16,12 @@ class Lockdown:
     async def lock(self, ctx, arg=""):
         """Lock message sending in the channel. Staff only."""
         try:
-            if(arg == 'community'):
+            if arg in {'community', 'cm'}:
                 role = self.bot.community_role
             else:
                 role = self.bot.everyone_role
-
             overwrites = ctx.message.channel.overwrites_for(role)
-
-            if(overwrites.send_messages is False):
+            if overwrites.send_messages is False:
                 await self.bot.say("🔒 Channel is already locked down. Use `.unlock [community]` to unlock.")
                 return
             overwrites.send_messages = False
@@ -36,20 +34,22 @@ class Lockdown:
         except discord.errors.Forbidden:
             await self.bot.say("💢 I don't have permission to do this.")
 
-
-
     @commands.has_permissions(manage_messages=True)
     @commands.command(pass_context=True, name="softlock")
     async def softlock(self, ctx):
         """Lock message sending in the channel, without the "disciplinary action" note. Staff only."""
         try:
-            overwrites_everyone = ctx.message.channel.overwrites_for(self.bot.everyone_role)
-            if overwrites_everyone.send_messages is False:
-                await self.bot.say("🔒 Channel is already locked down. Use `.unlock` to unlock.")
+            if arg in {'community', 'cm'}:
+                role = self.bot.community_role
+            else:
+                role = self.bot.everyone_role
+            overwrites = ctx.message.channel.overwrites_for(role)
+            if overwrites.send_messages is False:
+                await self.bot.say("🔒 Channel is already locked down. Use `.unlock [community]` to unlock.")
                 return
-            overwrites_everyone.add_reactions = False
-            overwrites_everyone.send_messages = False
-            await self.bot.edit_channel_permissions(ctx.message.channel, self.bot.everyone_role, overwrites_everyone)
+            overwrites.send_messages = False
+            overwrites.add_reactions = False
+            await self.bot.edit_channel_permissions(ctx.message.channel, role, overwrites)
             await self.bot.say("🔒 Channel locked.")
             msg = "🔒 **Soft-lock**: {0} by {1} | {2}#{3}".format(ctx.message.channel.mention, ctx.message.author.mention, ctx.message.author.name, ctx.message.author.discriminator)
             await self.bot.send_message(self.bot.modlogs_channel, msg)
@@ -61,14 +61,12 @@ class Lockdown:
     async def unlock(self, ctx, arg=""):
         """Unlock message sending in the channel. Staff only."""
         try:
-            if(arg == 'community'):
+            if arg == 'community':
                 role = self.bot.community_role
             else:
                 role = self.bot.everyone_role
-
             overwrites = ctx.message.channel.overwrites_for(role)
-
-            if(overwrites.send_messages is True):
+            if overwrites.send_messages is True:
                 await self.bot.say("🔓 Channel is already unlocked.")
                 return
             overwrites.send_messages = True
--- addons/mod.py
+++ addons/mod.py
@@ -169,7 +169,7 @@ class Mod:
     @commands.command(pass_context=True, name="reset")
     async def reset(self, ctx, limit: int = 100, force: str = ""):
         """Wipes messages in #newcomers and pastes the welcome message again. Staff only."""
-        if(ctx.message.channel != self.bot.welcome_channel and force != "force"):
+        if ctx.message.channel != self.bot.welcome_channel and force != "force":
             await self.bot.say("This command is limited to <#{}>".format(self.bot.welcome_channel.id))
             return
 
