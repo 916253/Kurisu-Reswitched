8538b88bbfe41a7367507e3c4f958078e2bd8c3d
roblabla <unfiltered@roblab.la>
2018-04-26 00:13:40 +0200

    Fix lockdown: use unprobated_role instead of everyone_role

--- addons/events.py
+++ addons/events.py
@@ -36,19 +36,19 @@ class Events:
     async def on_message(self, message):
         if message.mention_everyone:
             channel = message.channel
-            overwrites = channel.overwrites_for(self.bot.everyone_role)
+            overwrites = channel.overwrites_for(self.bot.unprobated_role)
             ow_old_value1 = overwrites.send_messages
             ow_old_value2 = overwrites.add_reactions
             if ow_old_value1 is False:
                 return
             overwrites.send_messages = False
             overwrites.add_reactions = False
-            await self.bot.edit_channel_permissions(channel, self.bot.everyone_role, overwrites)
+            await self.bot.edit_channel_permissions(channel, self.bot.unprobated_role, overwrites)
             await self.bot.send_message(self.bot.modlogs_channel, "🔒 **Auto-Lockdown**: {} auto-locked due to mention of everyone".format(channel.mention))
             await asyncio.sleep(60 * 2)
             overwrites.send_messages = ow_old_value1
             overwrites.add_reactions = ow_old_value2
-            await self.bot.edit_channel_permissions(channel, self.bot.everyone_role, overwrites)
+            await self.bot.edit_channel_permissions(channel, self.bot.unprobated_role, overwrites)
             await self.bot.send_message(self.bot.modlogs_channel, "🔒 **Auto-Unlock**: {}".format(channel.mention))
 
 
